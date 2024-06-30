b2f97314f8aba72a0214b30bee10e7542bd523b8
bob h <mail@bob.codes>
2024-06-30 12:45:09 +0100

    aspire dashboard (work so far)

--- TPLinkRelayApi.Api/Program.cs
+++ TPLinkRelayApi.Api/Program.cs
@@ -1,12 +1,64 @@
 using Helpers.Json.Converters;
 using Helpers.NetworkDiscovery;
 using Helpers.NetworkDiscovery.Concrete;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 using TPLinkRelayApi.Services;
 using TPLinkRelayApi.Services.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.Configure<OtlpExporterOptions>(
+	o => o.Headers = "x-otlp-api-key=c0b35c056105346aa1a89f3ce0bb8bf9c0c324182e40d4ec1a4e51a2a367a536");
+
+builder.Logging.AddOpenTelemetry(x =>
+{
+	x.IncludeScopes = true;
+	x.IncludeFormattedMessage = true;
+});
+
+builder.Services.AddOpenTelemetry()
+	.WithMetrics(x =>
+	{
+		x.AddRuntimeInstrumentation()
+			.AddMeter(
+				"Microsoft.AspNetCore.Hosting",
+				"Microsoft.AspNetCore.Server.Kestrel",
+				"System.Net.Http",
+				"TPLinkRelayApi.Api");
+	})
+	.WithTracing(x =>
+	{
+		if (builder.Environment.IsDevelopment())
+		{
+			x.SetSampler<AlwaysOnSampler>();
+		};
+
+		x.AddAspNetCoreInstrumentation()
+			.AddGrpcClientInstrumentation()
+			.AddHttpClientInstrumentation();
+	});
+
+builder.Services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddOtlpExporter());
+builder.Services.ConfigureOpenTelemetryMeterProvider(metrics => metrics.AddOtlpExporter());
+builder.Services.ConfigureOpenTelemetryTracerProvider(tracing => tracing.AddOtlpExporter());
+
+builder.Services.AddHealthChecks()
+	.AddCheck("self", () => HealthCheckResult.Healthy(), ["live",]);
+
+builder.Services.AddMetrics();
+
+builder.Services.ConfigureHttpClientDefaults(http =>
+{
+	http.AddStandardResilienceHandler();
+});
+
 builder.Services
 	.AddMemoryCache()
 	.AddTransient<HttpMessageHandler>(_ => new HttpClientHandler { AllowAutoRedirect = false, })
@@ -30,7 +82,16 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMetrics();
+
 var app = builder.Build();
+
+app.MapHealthChecks("/health");
+
+app.MapHealthChecks("/alive", new HealthCheckOptions
+{
+	Predicate = r => r.Tags.Contains("live"),
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
